e8e7e38e2dbd2fabefdda1c8094d575a4e9c8c4d
Sebastian Restrepo Mira <sebarestrepo97@hotmail.com>
2023-10-30 17:31:13 -0500

    Realizacion del body de la invitacion

---
+++ src/Hanna.jsx
@@ -0,0 +1,22 @@
+import './Hanna.css'
+
+function Hanna() {
+  
+  return (
+    <>
+      <section className='portada'>
+        <div className='circulo'>
+          <div className='foto'>
+            <img src="public/img/SebasDani.jpeg"></img>
+          </div>
+        </div>
+        <div className='informacion'>
+          <h1>Baby Shower</h1>
+          <img src='public/img/Hanna.png'></img>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default Hanna
